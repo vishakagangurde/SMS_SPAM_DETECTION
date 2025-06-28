3da9609b58780b1778980d4ebb7d28be46aa6e95
Vishaka <gvisha25@gmail.com>
2025-06-28 14:45:23 +0530

    commited

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
+
 mkdir -p ~/.streamlit/
 
-echo "\
-[server]\n\
-port = $PORT\n\
-enableCORS = false\n\
-headless = true\n\
-\n\
-" > ~/.streamlit/config.toml
+cat << EOF > ~/.streamlit/config.toml
+[server]
+port = ${PORT:-8501}
+enableCORS = false
+headless = true
+EOF
